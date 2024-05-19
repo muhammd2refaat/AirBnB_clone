023fe09a40af28c120304213a8ce764faf763b99
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-19 19:10:21 +0300

    Done_100%

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from models.engine import file_storage
+from models.engine.file_storage import FileStorage
 """ import FileStorage """
 # create an instance of FileStorage
-storage = file_storage.FileStorage()
+storage = FileStorage()
 # call reload()
 storage.reload()
