cfa6dd9160e854e32d9f43fd5f68d7cef545eafd
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-19 19:21:36 +0300

    update_Console

--- console.py
+++ console.py
@@ -5,23 +5,19 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """HBNB command interpreter."""
 
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, *arg):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, *arg):
         """EOF signal to exit the program."""
         return True
 
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
-
-    def default(self, arg):
-        """Default behavior for all other commands."""
-        cmd.Cmd.default(self, arg)
 
 
 if __name__ == '__main__':
