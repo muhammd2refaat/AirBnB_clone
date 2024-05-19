431ec04e00bd674f02613959a49801980ca10f8b
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-19 14:46:33 +0300

    File_Storage

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.engine.file_storage import FileStorage as Storage
+""" import FileStorage """
+# create an instance of FileStorage
+storage = Storage()
+# call reload()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+# from models.base_model import BaseModel
+# from models.user import User
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.place import Place
+# from models.review import Review
+
+class FileStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path for the JSON file
+        __objects: objects will be stored
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in FileStorage.__objects:
+            json_objects[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(json_objects, file)
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, 'r') as jason_file:
+                json_objects = json.load(jason_file)
+        except FileNotFoundError:
+            pass
+        else:
+            for key in json_objects:
+                self.__objects[key] = eval(json_objects[key]['__class__'])(**json_objects[key])
+                
