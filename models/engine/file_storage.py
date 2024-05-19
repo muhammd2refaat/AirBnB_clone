559dfe2e181e1445484ca728669a10b4fdaaba54
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-19 15:34:55 +0300

    File_Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,7 @@ import json
 # from models.place import Place
 # from models.review import Review
 
+
 class FileStorage:
     """This class serializes instances to a JSON file and
     deserializes JSON file to instances
@@ -22,26 +23,30 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
+
         return FileStorage.__objects
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
+
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
+
         for key in FileStorage.__objects:
             json_objects[key] = FileStorage.__objects[key].to_dict()
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(json_objects, file)
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, 'r') as jason_file:
-                json_objects = json.load(jason_file)
+            with open(FileStorage.__file_path, 'r') as file_jason:
+                jason_objects = json.load(file_jason)
         except FileNotFoundError:
-            pass
-        else:
-            for key in json_objects:
-                self.__objects[key] = eval(json_objects[key]['__class__'])(**json_objects[key])
-                
+            return
+        for key, value in jason_objects.items():
+            self.all()[key] = self.create_object(value["__class__"])(**value)
