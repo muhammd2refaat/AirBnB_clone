d9e75ca5b854e8b6b2fe48ec56065cf35a7cf60e
m7md3atef <m7md3atef@yahoo.com>
2024-05-20 03:22:24 +0300

    TASK 9

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Amenity class, that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity (models/amenity.py):
+
+    Public class attributes:
+        name: string - empty string
+
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+State class, that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State (models/state.py):
+
+    Public class attributes:
+    name: string - empty string
+    """
+    name = ""
